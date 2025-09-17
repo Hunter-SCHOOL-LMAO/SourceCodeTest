efd35dc60eab7f77ec2f8e4b9c659331523a3038
Hunter Madden <hmadden1@hwemail.com>
2025-09-16 20:20:40 -0700

    (0.4.1): Added a Tester File
    see description

--- MatrixExample.java
+++ MatrixExample.java
@@ -1,32 +1,5 @@
 import java.util.Random;
-
 public class MatrixExample {
-    public static void main(String[] args) {
-        int[][] matrix = {
-                { 1, 2, 3, 4, 5, 6 },
-                { 4, 5, 6, 3, 7, 2 },
-                { 27, 8, 9, 5, 3, 21 },
-                { 73, 2, 19, 5, 1, 8 },
-                { 47, 9, 9, 5, 0, 22 },
-                { 78, 86, 1, 4, 1, 21 },
-                { 73, 18, 2, 2, 5, 11 }
-        };
-
-        int numRows = 6;
-        int numCols = 7;
-
-        int[][] matrix2 = generateRandomMatrix(numRows, numCols);
-        int[][] result = multiplyMatrices(matrix, matrix2);
-
-        System.out.println("result length: " + result.length + " x " + result[0].length);
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[i].length; i++) {
-                System.out.print(result[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-    }
 
     public static int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2) {
         int rows1 = matrix1.length;
